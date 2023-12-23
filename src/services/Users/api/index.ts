6e23480f730b08482e568114ae8c8493f1ac47e3
Alexandr Sarioglo <asarioglo@gmail.com>
2023-12-22 18:05:14 -0600

    Add login route, with localization in mind
    Extracted all the strings into a separate file so it's easier to swap them
    both for localization and reuse of this application.
    Added a global icon and styles.

--- src/services/Users/api/index.ts
+++ src/services/Users/api/index.ts
@@ -2,13 +2,19 @@ import { Router } from "express";
 import GetUser from "./get-user";
 import IUserRepository from "../database/base/i-user-repository";
 import about from "./about";
+import login from "./login";
 import Logger from "../../../core/logging/logger";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function (userRepo: IUserRepository) {
     const router = Router();
 
     const _logger = Logger.getLogger("users-api");
     _logger.debug("registering users routes");
+
+    // AUTH
+    router.get("/login", login);
+
     router.get("/me", GetUser);
     router.get("/about", about);
 
---
+++ src/services/Users/api/tests/login.spec.ts
@@ -0,0 +1,58 @@
+/**
+ * @jest-environment ./src/core/testing/test-env-with-express-app.ts
+ */
+import "jest";
+import supertest from "supertest";
+import { StatusCodes } from "http-status-codes";
+import { IConfigProvider } from "../../../../core/interfaces/i-config-provider";
+import App from "../../../../App";
+import login from "../login";
+import { ILogger } from "../../../../core/logging/i-logger";
+import strings from "../../views/assets/text/en-us.json";
+
+describe("GET /users/login", () => {
+    let app!: App;
+    let config: IConfigProvider;
+    let uri_prefix: string | null = null;
+    let logger: ILogger;
+
+    beforeAll(async () => {
+        logger = globalThis.__logger as ILogger;
+    });
+
+    beforeEach(async () => {
+        logger.debug("[GET /users/login] Initializing app instance");
+        app = globalThis.__app as App;
+        config = globalThis.__configProvider as IConfigProvider;
+        uri_prefix = config.get("route_prefix", "");
+    });
+
+    afterEach(async () => {
+        logger.debug("[GET /users/login] Cleaning up app instance");
+        await app.stop();
+    });
+
+    it("should return a rendered login view", async () => {
+        logger.debug("[GET /users/login] should return 200 OK");
+        app.addCustomMiddleware("test-get-login", login);
+        logger.debug("[GET /users/login] Middleware registered");
+        await app.start(true);
+        logger.debug("[GET /users/login] App started");
+        const response = await supertest(app.getExpressApp())
+            .get(`${uri_prefix}/test-get-login`)
+            .set("Accept", "*")
+            .expect((res) => {
+                if (res.status !== StatusCodes.OK) {
+                    logger.error(`${res.status} ${res.text}`);
+                }
+            })
+            .expect(StatusCodes.OK)
+            .expect("Content-Type", /html/);
+
+        const authStrings = strings.auth;
+        for (const s in authStrings) {
+            expect(response.text).toContain(authStrings[s]);
+        }
+        logger.debug("[GET /users/login] Request completed");
+    });
+});
